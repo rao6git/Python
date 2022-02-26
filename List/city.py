8f26e1bbeecfccca7a6a3c5529fdbb9b396bbe51
Naga <nagasr.mca@gmail.com>
2022-02-26 13:04:47 +0530

    naga moved

---
+++ List/city.py
@@ -0,0 +1,6 @@
+city =['hyderabad','bhimavaram','bangalore']
+city=input("enter city name")
+if city=="city":
+    Print("he is in {}".format(city))
+else:
+    print("he is no here")  
---
+++ List/naga.py
@@ -0,0 +1,6 @@
+ Naga, storage and python Automation Engineer 
+:   i=1
+    for i in range(10):
+        i+=1
+        print(i)            :
+    
